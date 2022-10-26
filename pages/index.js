686048de4efc264c8d4d6ab1ca2c30e736e81c51
WMs784 <wms784.app@gmail.com>
2022-10-26 12:55:24 +0900

    get data from spreadsheet

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home({posts}) {
+export default function Home({loc_list}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,50 +16,11 @@ export default function Home({posts}) {
           白山祭実行委員会　居場所一覧
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <p>{posts}</p>
-
-        {/* <ul>
-          {posts.map((post) => {
-            return <li key={post.name}>{post.room}</li>;
+        <ul>
+          {loc_list.map((loc) => {
+            return <li key={loc.time}>{`氏名：${loc.name}，現在地：${loc.room}`}</li>;
           })}
-        </ul> */}
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -76,4 +37,12 @@ export default function Home({posts}) {
       </footer>
     </div>
   )
+}
+export async function getServerSideProps() {
+  const key = process.env.KEY;
+  const lib = process.env.LIB;
+  const res = await fetch(`https://script.googleusercontent.com/macros/echo?user_content_key=${key}&lib=${lib}`);
+  const loc_list = await res.json();
+  console.log(loc_list);
+  return { props: { loc_list } };
 }
